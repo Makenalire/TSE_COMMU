3c9976965bee1a0eddf6d2e0ef162b938f5715e7
chawincode <chawin.suw@dome.tu.ac.th>
2022-11-19 22:17:27 +0700

    end

--- screens/RoomTime.js
+++ screens/RoomTime.js
@@ -1,34 +1,28 @@
-import React, {useState} from 'react';
-import {Button, View, TouchableOpacity, Text, StyleSheet, Alert} from 'react-native';
-import {Agenda} from 'react-native-calendars';
-import {Card} from 'react-native-paper';
-import { useSelector, useDispatch } from 'react-redux';
-import { setRoom, setRoomDate, setRoomTime } from '../redux/action';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState, useEffect, useRef } from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { Agenda } from "react-native-calendars";
+import { Card } from "react-native-paper";
+import { useSelector, useDispatch } from "react-redux";
+import { setRoomDate, setRoomTime, reset } from "../redux/action";
+import { useNavigation } from "@react-navigation/native";
+import { setDoc, doc, getDoc, updateDoc, arrayUnion } from "firebase/firestore";
+import { db } from "../services/firebaseCovid";
 
 const timeToString = (time) => {
   const date = new Date(time);
-  return date.toISOString().split('T')[0];
+  return date.toISOString().split("T")[0];
 };
 
-const Schedule = () => {
+const RoomTime = () => {
+  const [isLoading, setLoading] = useState(true);
+  const roomTimeArray = useRef({});
   const [items, setItems] = useState({});
   const navigation = useNavigation();
-  const { room } = useSelector(state => state.userReducer);
+  const { room, roomTime, roomDate } = useSelector(
+    (state) => state.userReducer
+  );
+  const [reload, setReload] = useState(false);
   const dispatch = useDispatch();
-  const createTwoButtonAlert = () =>
-    Alert.alert(
-      'complete!',
-      // [
-      //   {
-      //     text: "Cancel",
-      //     onPress: () => console.log("Cancel Pressed"),
-      //     style: "cancel"
-      //   },
-      //   { text: "OK", onPress: () => console.log("OK Pressed") }
-      // ]
-    );
-
   const DateLists = [
     { id: 1, event: "08:00 - 10:00" },
     { id: 2, event: "10:00 - 12:00" },
@@ -38,34 +32,42 @@ const Schedule = () => {
     { id: 6, event: "18:00 - 20:00" },
     { id: 7, event: "20:00 - 22:00" },
     { id: 8, event: "22:00 - 00:00" },
-    // { id: 9, event: "08:00 - 10:00" },
-    // { id: 10, event: "10:00 - 12:00" },
-    // { id: 11, event: "12:00 - 14:00" },
-    // { id: 12, event: "14:00 - 16:00" },
-    // { id: 13, event: "16:00 - 18:00" },
-    // { id: 14, event: "18:00 - 20:00" },
-    // { id: 15, event: "20:00 - 22:00" },
-    // { id: 16, event: "22:00 - 00:00" }
   ];
+
+  async function getInfo() {
+    const roomReserve = doc(db, "roomdata", room);
+    const roomRef = await getDoc(roomReserve);
+    const info = roomRef.get("roomdata");
+    console.log(info);
+    for (let i = 0; i < info.length; i++) {
+      var temp = roomTimeArray.current[info[i].date];
+
+      if (temp == undefined) {
+        roomTimeArray.current[info[i].date] = [info[i].time];
+      } else {
+        roomTimeArray.current[info[i].date] = [...temp, info[i].time];
+      }
+    }
+    console.log(roomTimeArray.current);
+  }
 
   const loadItems = (day) => {
     setTimeout(() => {
       for (let i = -15; i < 85; i++) {
         const time = day.timestamp + i * 24 * 60 * 60 * 1000;
         const strTime = timeToString(time);
-          if (!items[strTime]) {
-            items[strTime] = [];
-            const numItems = Math.floor(1);
-            for (let j = 0; j < numItems; ++j) {
-              DateLists.map((product) => {
-                items[strTime].push({
-                  time: product.event,
-                  date: strTime
-                })
-              } 
-              )
-            }
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(1);
+          for (let j = 0; j < numItems; ++j) {
+            DateLists.map((product) => {
+              items[strTime].push({
+                time: product.event,
+                date: strTime,
+              });
+            });
           }
+        }
       }
       const newItems = {};
       Object.keys(items).forEach((key) => {
@@ -73,21 +75,105 @@ const Schedule = () => {
       });
       // console.log(newItems);
       // dispatch(setRoom(newItems))
-      setItems(newItems)
-    }, 1000);
+      setItems(newItems);
+    }, 10);
   };
 
+  useEffect(() => {
+    if (isLoading == true) {
+      getInfo().then(setLoading(false));
+    }
+    handlerAddRoom();
+  });
+
+  const handlerAddRoom = async () => {
+    console.log(room + "hihi");
+    if (roomDate == "" || roomTime == "") {
+      await roomDate;
+      await roomTime;
+    } else {
+      const roomInfo = doc(db, "roomdata", room);
+      const docSnap = await getDoc(roomInfo);
+
+      if (docSnap.exists()) {
+        await updateDoc(roomInfo, {
+          roomdata: arrayUnion({
+            date: roomDate,
+            time: roomTime,
+          }),
+        });
+        navigation.goBack(
+          alert(
+            "Book Exist! Try another room or time\n" +
+              "Room: วศ." +
+              room +
+              "\n" +
+              "Date: " +
+              roomDate +
+              "\n" +
+              "Time: " +
+              roomTime
+          )
+        );
+      } else {
+        await setDoc(roomInfo, {
+          roomdata: [
+            {
+              date: roomDate,
+              time: roomTime,
+            },
+          ],
+        });
+        navigation.goBack(
+          alert(
+            "Book Complete!\n" +
+              "Room: วศ." +
+              room +
+              "\n" +
+              "Date: " +
+              roomDate +
+              "\n" +
+              "Time: " +
+              roomTime
+          )
+        );
+      }
+    }
+    // navigation.navigate("Room", console.log(room))
+    setReload(true);
+    // navigation.navigate.goBack(console.log(room))
+  };
   const renderItem = (item) => {
     return (
-      <TouchableOpacity style={{marginRight: 10, marginTop: 17 ,top: 12}} onPress={() => navigation.navigate("Room", dispatch(setRoomTime(item.time)), dispatch(setRoomDate(item.date)))}>
+      <TouchableOpacity         
+      disabled={
+        roomTimeArray.current[item.date] == undefined
+          ? false
+          : roomTimeArray.current[item.date].includes(item.time)
+          ? true
+          : false
+      }
+        style={{ marginRight: 10, marginTop: 17, top: 12 }}
+        onPress={() => (
+          dispatch(setRoomDate(item.date)), dispatch(setRoomTime(item.time))
+        )}
+      >
         <Card>
-          <Card.Content>
+          <Card.Content
+            style={
+              roomTimeArray.current[item.date] == undefined 
+                ? styles.CardBackground
+                :roomTimeArray.current[item.date].includes(item.time) ? styles.CardBackgroundDisable 
+                :styles.CardBackground
+            }
+          >
             <View
               style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignSelf: 'center',
-              }}>
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignSelf: "center",
+              }}
+            >
               <Text>{item.time}</Text>
             </View>
           </Card.Content>
@@ -95,31 +181,36 @@ const Schedule = () => {
       </TouchableOpacity>
     );
   };
-
   return (
-    <View style={{flex: 1}}>
-      <Agenda
-        items={items}
-        loadItemsForMonth={loadItems}
-        selected={Date.now()}
-        renderItem={renderItem}
-        showClosingKnob={true}
-        theme={{
-          agendaDayTextColor: "darkorange",
-          agendaDayNumColor: "#3c3c3c",
-          agendaKnobColor: "lightgrey",
-          backgroundColor: "#fffbed",
-          monthTextColor: "darkorange",
-          textSectionTitleColor: "darkorange",
-          dayTextColor: "#5d6e1e",
-          selectedDayBackgroundColor: "darkorange",
-          todayTextColor: "red",
-          dotColor: "white",
-          selectedDotColor: "darkorange",
-        }}
-        pastScrollRange={1}
-        futureScrollRange={1}
-      />
+    <View style={{ flex: 1 }}>
+      {!isLoading ? (
+        <Agenda
+          items={items}
+          loadItemsForMonth={loadItems}
+          selected={Date.now()}
+          renderItem={renderItem}
+          showClosingKnob={true}
+          theme={{
+            agendaDayTextColor: "darkorange",
+            agendaDayNumColor: "#3c3c3c",
+            agendaKnobColor: "lightgrey",
+            backgroundColor: "#fffbed",
+            monthTextColor: "darkorange",
+            textSectionTitleColor: "darkorange",
+            dayTextColor: "#5d6e1e",
+            selectedDayBackgroundColor: "darkorange",
+            todayTextColor: "red",
+            dotColor: "white",
+            selectedDotColor: "darkorange",
+          }}
+          pastScrollRange={1}
+          futureScrollRange={1}
+        />
+      ) : (
+        <View>
+          <Text style={{ textAlign: "center" }}>Loading...</Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -128,25 +219,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "space-around",
-    alignItems: "center"
+    alignItems: "center",
   },
   fab: {
     left: 24,
     top: 10,
     width: 190,
     height: 40,
-    backgroundColor: '#efddc6',
+    backgroundColor: "#efddc6",
     borderRadius: 9,
-    position: "absolute"
+    position: "absolute",
   },
   fabIcon: {
-      left: 40,
-      top: 10,
-      fontSize: 18,
-      color: '#6b6b6b',
+    left: 40,
+    top: 10,
+    fontSize: 18,
+    color: "#6b6b6b",
   },
   FlatListCont: {
-    flex: .5,
+    flex: 0.5,
   },
 
   image: {
@@ -160,11 +251,15 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     alignSelf: "center",
     justifyContent: "center",
-    paddingLeft: 15
+    paddingLeft: 15,
   },
   outerCard: {
     padding: 10,
   },
+  CardBackgroundDisable: {
+    backgroundColor: "#d9d9d9",
+    opacity: 10
+  },
 });
 
-export default Schedule;
+export default RoomTime;
--- screens/Students/Room.js
+++ screens/Students/Room.js
@@ -1,145 +1,235 @@
-import React, { useEffect } from "react";
-import { View, ImageBackground, StyleSheet, TouchableOpacity, Image, FlatList, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { useState } from "react";
-import { useFonts } from "expo-font";
-import { useSelector, useDispatch } from 'react-redux';
-import { setRoom } from '../../redux/action';
+import React from "react";
+import {
+  View,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Text,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch } from "react-redux";
+import { setRoom , setRoomDate, setRoomTime } from "../../redux/action";
 
-const image = require('../../assets/Background.jpg')
+
+const image = require("../../assets/Background.jpg");
 
 export default function Room() {
-  const { room, roomTime, roomDate } = useSelector(state => state.userReducer);
+  // const { room, roomTime, roomDate } = useSelector(
+  //   (state) => state.userReducer
+  // );
   const dispatch = useDispatch();
-    const navigation = useNavigation();
-    const roomData = [
-      {name: 'PRIVATE ROOM ', id: '200', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-      {name: 'PRIVATE ROOM ', id: '201', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-      {name: 'PRIVATE ROOM ', id: '300', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-      {name: 'PRIVATE ROOM ', id: '301', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-      {name: 'PRIVATE ROOM ', id: '400', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-      {name: 'PRIVATE ROOM ', id: '401', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-      {name: 'PRIVATE ROOM ', id: '500', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-      {name: 'PRIVATE ROOM ', id: '501', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-      {name: 'PRIVATE ROOM ', id: '600', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-      {name: 'PRIVATE ROOM ', id: '601', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-      {name: 'PRIVATE ROOM ', id: '700', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-      {name: 'PRIVATE ROOM ', id: '701', type: 'ROOM TYPE PRIVATE', cap: 'CAPACITY', size: '4-6 PERSONS'},
-    ]
+  const navigation = useNavigation();
 
-    useEffect(() => {
-      console.log(room)
-      console.log(roomTime)
-      console.log(roomDate)
-    })
+  const roomData = [
+    {
+      name: "PRIVATE ROOM ",
+      id: "200",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+    {
+      name: "PRIVATE ROOM ",
+      id: "201",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+    {
+      name: "PRIVATE ROOM ",
+      id: "300",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+    {
+      name: "PRIVATE ROOM ",
+      id: "301",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+    {
+      name: "PRIVATE ROOM ",
+      id: "400",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+    {
+      name: "PRIVATE ROOM ",
+      id: "401",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+    {
+      name: "PRIVATE ROOM ",
+      id: "500",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+    {
+      name: "PRIVATE ROOM ",
+      id: "501",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+    {
+      name: "PRIVATE ROOM ",
+      id: "600",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+    {
+      name: "PRIVATE ROOM ",
+      id: "601",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+    {
+      name: "PRIVATE ROOM ",
+      id: "700",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+    {
+      name: "PRIVATE ROOM ",
+      id: "701",
+      type: "ROOM TYPE PRIVATE",
+      cap: "CAPACITY",
+      size: "4-6 PERSONS",
+    },
+  ];
 
-    return (
-      <View style={styles.view}>
-        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+  const setR = (item) => {
+    dispatch(setRoom(item.id))
+    dispatch(setRoomTime(''))
+    dispatch(setRoomDate(''))
+    navigation.navigate("RoomTime")
+  }
+
+  // useEffect(() => {
+  //   console.log(room);
+  //   // console.log(roomTime);
+  //   // console.log(roomDate);
+  // });
+
+  return (
+    <View style={styles.view}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
         <Text style={styles.textPet}>EXPLORE SPACE</Text>
-            <FlatList 
-              data={roomData}
-              numColumns={2}
-              renderItem= {({ item }) => (             
-                  <TouchableOpacity style={styles.boxin1} onPress={() => navigation.navigate('RoomTime', dispatch(setRoom(item.id)))}>
-                      <View style={styles.fab}>
-                        <Text style={styles.fabIcon}>BOOK NOW</Text>
-                      </View>
-                      <Text style={styles.text1}>{item.name + item.id}</Text>
-                      <Text style={styles.text2}>{item.type}</Text>
-                      <Text style={styles.text3}>{item.cap}</Text>
-                      <Text style={styles.text3}>{item.size}</Text>
-                  </TouchableOpacity>
-                // <View style={styles.boxin1}>
-                //   <Text style={styles.text1}>{item.name}</Text>
-                //   <Text style={styles.text2}>{item.type}</Text>
-                //   <Text style={styles.text3}>ตึกวิศวกรรมศาสตร์</Text>
-                //   <Text style={styles.text3}>{item.cap}</Text>
-                //   <Text style={styles.text3}>{item.size}</Text>
-                // </View>
-              )}
-              ListFooterComponent={() => (
-                <Text style={{ marginBottom:80 }}></Text>
-              )}
-            />
-            </ImageBackground>
-          </View>
-      )}
+        <FlatList
+          data={roomData}
+          numColumns={2}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.boxin1}
+              onPress={() => setR(item)}
+            >
+              <View style={styles.fab}>
+                <Text style={styles.fabIcon}>BOOK NOW</Text>
+              </View>
+              <Text style={styles.text1}>{item.name + item.id}</Text>
+              <Text style={styles.text2}>{item.type}</Text>
+              <Text style={styles.text3}>{item.cap}</Text>
+              <Text style={styles.text3}>{item.size}</Text>
+            </TouchableOpacity>
+            // <View style={styles.boxin1}>
+            //   <Text style={styles.text1}>{item.name}</Text>
+            //   <Text style={styles.text2}>{item.type}</Text>
+            //   <Text style={styles.text3}>ตึกวิศวกรรมศาสตร์</Text>
+            //   <Text style={styles.text3}>{item.cap}</Text>
+            //   <Text style={styles.text3}>{item.size}</Text>
+            // </View>
+          )}
+          ListFooterComponent={() => <Text style={{ marginBottom: 80 }}></Text>}
+        />
+      </ImageBackground>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   fab: {
-      alignSelf: 'center',
-      top: 15,
-      width: 90,
-      height: 23,
-      backgroundColor: '#efddc6',
-      borderRadius: 9,
-      position: "absolute"
+    alignSelf: "center",
+    top: 15,
+    width: 90,
+    height: 23,
+    backgroundColor: "#efddc6",
+    borderRadius: 9,
+    position: "absolute",
   },
   fabIcon: {
-      alignSelf: 'center',
-      top: 2,
-      fontSize: 13,
-      color: '#6b6b6b'
+    alignSelf: "center",
+    top: 2,
+    fontSize: 13,
+    color: "#6b6b6b",
   },
   view: {
-      flex: 1
+    flex: 1,
   },
   image: {
-      flex: 1,
+    flex: 1,
   },
   text1: {
-      left: 10,
-      top: 45,
-      color: 'white', 
-      fontSize: 13
+    left: 10,
+    top: 45,
+    color: "white",
+    fontSize: 13,
   },
   text2: {
-      left: 10,
-      top: 48,
-      color: '#F1ADAD', 
-      fontSize: 10
+    left: 10,
+    top: 48,
+    color: "#F1ADAD",
+    fontSize: 10,
   },
   text3: {
-      left: 10,
-      top: 63,
-      color: 'white', 
-      fontSize: 13
+    left: 10,
+    top: 63,
+    color: "white",
+    fontSize: 13,
   },
   text4: {
-      left: 10,
-      top: 64,
-      color: 'white', 
-      fontSize: 13
+    left: 10,
+    top: 64,
+    color: "white",
+    fontSize: 13,
   },
   textPet: {
-      position: "absolute",
-      left: 31,
-      top: 50,
-      width: 178,
-      height: 26,
-      fontFamily: 'AbhayaLibre-Medium',
-      fontSize: 20,
-      fontWeight: "400",
-      fontStyle: "normal",
-      lineHeight: 20,
-      color: "#100F0F",
+    position: "absolute",
+    left: 31,
+    top: 50,
+    width: 178,
+    height: 26,
+    fontFamily: "AbhayaLibre-Medium",
+    fontSize: 20,
+    fontWeight: "400",
+    fontStyle: "normal",
+    lineHeight: 20,
+    color: "#100F0F",
   },
   btnRoom: {
-      height: 26.04,
-      width: 25,
-      alignSelf: 'center',
-      top: 7
+    height: 26.04,
+    width: 25,
+    alignSelf: "center",
+    top: 7,
   },
   boxin1: {
-      width: 140,
-      height: 140,
-      backgroundColor: "#6b6b6b",
-      borderRadius: 10,
-      marginRight: 20,
-      marginBottom: -10,
-      left: 30, 
-      top: 70, 
-      marginTop: 30
-  }
-})
+    width: 140,
+    height: 140,
+    backgroundColor: "#6b6b6b",
+    borderRadius: 10,
+    marginRight: 20,
+    marginBottom: -10,
+    left: 30,
+    top: 70,
+    marginTop: 30,
+  },
+});
